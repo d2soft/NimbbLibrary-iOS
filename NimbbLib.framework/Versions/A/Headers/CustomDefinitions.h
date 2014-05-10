2c1ead269aecdfa67617dc363a814ccddff84670
Vibr8Soft <info@vibr8soft.com>
2014-05-10 14:11:39 -0400

    0.9.3

--- NimbbLib.framework/Versions/A/Headers/CustomDefinitions.h
+++ NimbbLib.framework/Versions/A/Headers/CustomDefinitions.h
@@ -9,3 +9,17 @@ typedef void (^ConfigurationCompletedBlock)(void);
 typedef void (^VideoSavedBlock)(NSString *);
 typedef void (^VideoUploadProgressionBlock)(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite);
 typedef void (^VideoWasCanceledBlock)(void);
+
+
+#ifndef SINGLETON_GCD
+#define SINGLETON_GCD(classname)                        \
+\
++ (classname *)sharedInstance {                         \
+\
+static dispatch_once_t pred;                        \
+__strong static classname * sharedInstance = nil;   \
+dispatch_once( &pred, ^{                            \
+sharedInstance = [[self alloc] init]; });       \
+return sharedInstance;                              \
+}
+#endif
